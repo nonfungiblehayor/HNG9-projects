40cc51a76951e2186624b1e4f9074b1e21704f94
unknown <nonfungibleayo.eth@gmail.com>
2022-10-29 14:43:39 +0100

    redfined access to dom element

--- first task/my-app/src/components/header.js
+++ first task/my-app/src/components/header.js
@@ -1,24 +1,25 @@
-import React from 'react' 
+import React, { useRef } from 'react' 
 import img from '../img.png';
 import Icon from '../Icon.png'
 import shareButton from '../share button.png'
 import camera from '../camera.png'
 
 function Header() {
+    const divRef = useRef()
 
-const hoverClass = document.querySelector('.hoverClass')
+// const hoverClass = document.querySelector('.hoverClass')
 
 const changeDisplay = function() {
-    hoverClass.style.display = 'flex'
+    divRef.current.style.display = 'flex'
   }
 const removeDisplay = function() {
-    hoverClass.style.display = 'none'
+    divRef.current.style.display = 'none'
   }
 
     return  <header className="App-header">
         <div className="div1">
       <img src={img} className="App-logo" alt="profile" id='profile__img' onMouseOut={removeDisplay} onMouseOver={changeDisplay}></img>
-      <div className="hoverClass"><img src={camera} alt="profile_hover"></img></div>
+      <div className="hoverClass" ref={divRef}><img src={camera} alt="profile_hover"></img></div>
             <p id="twitter" title='ayo'>0x_founder</p>
             <p id="slack">nonfungibleayo.eth</p>
         </div>
