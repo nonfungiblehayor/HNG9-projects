08273c607aa59b70ad8c47decfca3087772b3d3d
EIbrahim Ayodeji <nonfungiblehayor.eth@gmail.com>
2022-10-31 11:07:57 +0100

    Added subtext to the links

--- first task/my-app/src/components/profile.js
+++ first task/my-app/src/components/profile.js
@@ -8,9 +8,9 @@ class Profile extends Component {
       <a href='' target="_blank" id="btn__zuri">Twitter link</a>
       <a href='https://training.zuri.team/' target="_blank" id="btn__zuri">Zuri Team</a>
       <a href='http://books.zuri.team/' title='Find the best books on design & coding' target="_blank" id="btn__zuri">Zuri Books</a>
-      <a href='https://books.zuri.team/python-for-beginners?ref_id=<nonfungibleayo.eth>' target="_blank" id="book__python">Python Books</a>
-      <a href='https://background.zuri.team' target="_blank" id="pitch">Background check for coders</a>
-      <a href='https://books.zuri.team/design-rules' target="_blank" id="bok_design">Design Books</a>
+      <a href='https://books.zuri.team/python-for-beginners?ref_id=<nonfungibleayo>' title='Get the best book to get you started with python developement at a cheap price' target="_blank" id="book__python">Python Books</a>
+      <a href='https://background.zuri.team' title='We offer background check as a service for coders' target="_blank" id="pitch">Background check for coders</a>
+      <a href='https://books.zuri.team/design-rules' title='Get free design books to kickstart your desginer journey for free from Zuri' target="_blank" id="bok_design">Design Books</a>
       <div className="div3">
         <img src={slack} className="slack-logo" alt="slack" id='slack__img'></img>
         <img src={github} className="github-logo" alt="Github" id='github__img'></img>        
